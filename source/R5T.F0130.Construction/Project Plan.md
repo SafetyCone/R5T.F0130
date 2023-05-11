014147bde1e91ba2f4fde94e5db23dfaa77b21c2
davidcoats <david@getsafetycone.com>
2023-05-10 21:45:28 -0700

    Initial commit.

---
+++ source/R5T.F0130.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.F0130.Construction
+Construction console project for the R5T.F0130 library.
---
+++ source/R5T.F0130/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0130
+{
+	/// <summary>
+	/// Library file and directory path conventions.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
